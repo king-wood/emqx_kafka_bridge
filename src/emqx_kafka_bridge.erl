1a92b3657c3cd53f52b0a7f2208b8cfd409fbec0
zhanglin <lynn.zhang@ucloud.cn>
2019-04-15 17:51:44 +0800

    test

--- src/emqx_kafka_bridge.erl
+++ src/emqx_kafka_bridge.erl
@@ -116,10 +116,8 @@ on_message_publish(Message, _Env) ->
 
 on_message_delivered(#{client_id := ClientId, username := Username}, Message, _Env) ->
     % io:format("delivered to client(~s/~s): ~s~n", [Username, ClientId, emqttd_message:format(Message)]),
-    Message = Message#message{payload="delivered"},
     Event = [{clientid, ClientId},
                 {username, Username},
-                {id, Message#message.id},
                 {topic, Message#message.topic},
                 {size, byte_size(Message#message.payload)},
                 {ts, emqx_time:now_secs(Message#message.timestamp)}],
