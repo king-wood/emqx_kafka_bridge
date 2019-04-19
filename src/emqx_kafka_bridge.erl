e4a24cc6617dc352f8e81d2fe725d69216453245
zhanglin <lynn.zhang@ucloud.cn>
2019-04-19 18:53:13 +0800

    fix issue

--- src/emqx_kafka_bridge.erl
+++ src/emqx_kafka_bridge.erl
@@ -86,8 +86,7 @@ on_client_unsubscribe(#{client_id := ClientId, username := Username}, TopicTable
     {ok, TopicTable}.
 
 on_session_created(#{client_id := ClientId}, SessAttrs, _Env) ->
-    [_, _, _, UserNameAttr | _] = SessAttrs,
-    {_, UserName} = UserNameAttr,
+    [_, _, _, {_, Username} | _] = SessAttrs,
     io:format("session(~s/~s) created~n", [ClientId, Username]),
     Event = [{clientid, ClientId},
                 {username, Username},
@@ -95,7 +94,7 @@ on_session_created(#{client_id := ClientId}, SessAttrs, _Env) ->
     produce_kafka_session_created(Event).
 
 on_session_terminated(#{client_id := ClientId, username := Username}, _ReasonCode, _Env) ->
-    io:format("Session(~s/~s) terminated: .", [ClientId, Username]).
+    io:format("Session(~s/~s) terminated: .", [ClientId, Username]),
     Event = [{clientid, ClientId},
                 {username, Username},
                 {ts, timestamp()}],
